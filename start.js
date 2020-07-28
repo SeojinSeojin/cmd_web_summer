d01e8ccb854c221715865eede4642e02f77e15e5
SeojinSeojin <1106laura@naver.com>
2020-07-28 18:33:33 +0900

    router 추가. 노드 시작!

---
+++ start.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("index.html"));
+app.get("/main/", (req, res) => res.render("main.html"));
+app.get("/user/profile/change/", (req, res) =>
+    res.render("change_profile.html")
+);
+app.get("/user/profile/", (req, res) => res.render("user_profile.html"));
+app.get("/user/signup/", (req, res) => res.render("signup.html"));
+
+const PORT = 3000;
+app.listen(PORT, function() {
+    console.log(`Server is listening on ${PORT}`);
+    console.log(`http://localhost:${PORT}/`);
+});
