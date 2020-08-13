a36ff82a245b73bd42494f1dfae3fcaf1e8f5928
SeojinSeojin <1106laura@naver.com>
2020-08-13 23:57:44 +0900

    posting router이랑 controller 분리

---
+++ controllers/posting_controller.js
@@ -0,0 +1,46 @@
+import PostingModel from "../db/posting";
+import UserModel from "../db/user";
+
+export const postCreatePosting = (req, res) => {
+    const posting = new PostingModel({
+        title: req.body.title,
+        content: req.body.content,
+        creator: req.user._id,
+        ifAnonymous: req.ifAnonymous ? true : false,
+    });
+
+    posting.save(function(err) {
+        if (err) {
+            console.log(err);
+            res.render("posting_upload.ejs");
+        } else {
+            res.redirect(`/posting/detail/${posting._id}`);
+        }
+    });
+};
+
+export const getPostingDetail = (req, res) => {
+    PostingModel.findOne({ _id: req.params.id }).exec(function(
+        err,
+        postingmodel
+    ) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(postingmodel);
+            UserModel.findOne({
+                _id: postingmodel.creator,
+            }).exec(function(err, post_creator) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    const post_creator_name = post_creator.name;
+                    res.render("posting_detail.ejs", {
+                        post: postingmodel,
+                        creator: post_creator_name,
+                    });
+                }
+            });
+        }
+    });
+};
--- routers/posting_router.js
+++ routers/posting_router.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { isAuthenticated } from "../middleware";
-import PostingModel from "../db/posting";
-import UserModel from "../db/user";
+import {
+    postCreatePosting,
+    getPostingDetail,
+} from "../controllers/posting_controller";
 
 const posting_router = express.Router();
 
@@ -9,49 +11,8 @@ posting_router.get("/create/", isAuthenticated, (req, res) =>
     res.render("posting_upload.ejs")
 );
 
-posting_router.post("/create/", isAuthenticated, (req, res) => {
-    const posting = new PostingModel({
-        title: req.body.title,
-        content: req.body.content,
-        creator: req.user._id,
-        ifAnonymous: req.ifAnonymous ? true : false,
-    });
+posting_router.post("/create/", isAuthenticated, postCreatePosting);
 
-    posting.save(function(err) {
-        if (err) {
-            console.log(err);
-            res.render("posting_upload.ejs");
-        } else {
-            res.redirect(`/posting/detail/${posting._id}`);
-        }
-    });
-});
-
-posting_router.get("/detail/:id", (req, res) => {
-    PostingModel.findOne({ _id: req.params.id }).exec(function(
-        err,
-        postingmodel
-    ) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(postingmodel);
-            UserModel.findOne({
-                _id: postingmodel.creator,
-            }).exec(function(err, post_creator) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    const post_creator_name = post_creator.name;
-                    console.log(post_creator_name);
-                    res.render("posting_detail.ejs", {
-                        post: postingmodel,
-                        creator: post_creator_name,
-                    });
-                }
-            });
-        }
-    });
-});
+posting_router.get("/detail/:id", isAuthenticated, getPostingDetail);
 
 module.exports = posting_router;
